1c260daff810a329c17e547251caf6a4f2d97905
Alexisfllv <alexis-fl-live@hotmail.com>
2025-06-12 12:31:53 -0500

    feat (service,impl,controller) : agregando servicio de paginado lista sale

--- src/main/java/sora/com/saleapi/controller/SaleController.java
+++ src/main/java/sora/com/saleapi/controller/SaleController.java
@@ -2,11 +2,17 @@ package sora.com.saleapi.controller;
 
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sora.com.saleapi.dto.SaleDTO.SaleDTORequest;
 import sora.com.saleapi.dto.SaleDTO.SaleDTOResponse;
+import sora.com.saleapi.entity.Sale;
 import sora.com.saleapi.service.SaleService;
 
 import java.util.List;
@@ -22,6 +28,19 @@ public class SaleController {
     @GetMapping
     public ResponseEntity<List<SaleDTOResponse>> findAll(){
         List<SaleDTOResponse> lista = saleService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body( lista);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<SaleDTOResponse>> findAllPage(
+            @RequestParam (defaultValue = "0") int page,
+            @RequestParam (defaultValue = "5") int size,
+            @RequestParam (defaultValue = "saleId") String sort,
+            @RequestParam (defaultValue = "ASC") String direction
+    ) {
+        Sort sortOrder = Sort.by(Sort.Direction.fromString(direction), sort);
+        Pageable pageable = PageRequest.of(page, size, sortOrder);
+        Page<SaleDTOResponse> lista = saleService.findAllPage(pageable);
         return ResponseEntity.status(HttpStatus.OK).body( lista);
     }
 
