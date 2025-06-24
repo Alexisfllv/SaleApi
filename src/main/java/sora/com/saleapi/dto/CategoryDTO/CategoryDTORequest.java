f9007baf20d2898c1ba00d7c17caf0526afdca47
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-06-23 22:48:43 -0500

    feat (CategoryDTORequest) : agregando validaciones de no empty para los campos.

--- src/main/java/sora/com/saleapi/dto/CategoryDTO/CategoryDTORequest.java
+++ src/main/java/sora/com/saleapi/dto/CategoryDTO/CategoryDTORequest.java
@@ -1,18 +1,22 @@
 package sora.com.saleapi.dto.CategoryDTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CategoryDTORequest(
         @NotNull(message = "El nombre de la categoría es obligatorio")
+        @NotEmpty
         @Size(max = 50)
         String categoryName,
 
         @NotNull
+        @NotEmpty
         @Size(max = 250)
         String categoryDescription,
 
         @NotNull
+        @NotEmpty
         Boolean categoryEnabled
 ) {}
