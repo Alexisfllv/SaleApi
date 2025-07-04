6c8cec3e5d1f92a8be5ad291b7d1ddf4e76394d9
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-07-03 22:42:59 -0500

    fix (roleDTORequest) : modificando validaciones.

--- src/main/java/sora/com/saleapi/dto/RoleDTO/RoleDTORequest.java
+++ src/main/java/sora/com/saleapi/dto/RoleDTO/RoleDTORequest.java
@@ -2,11 +2,13 @@ package sora.com.saleapi.dto.RoleDTO;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RoleDTORequest(
         @NotBlank(message = "{field.required}")
         @Size(max = 50, min = 2, message = "{field.size.range}")
+        @Pattern(regexp = "^[\\p{L} ]+$", message = "{field.invalid.format}")
         String roleName,
 
         @NotNull(message = "{field.required}")
