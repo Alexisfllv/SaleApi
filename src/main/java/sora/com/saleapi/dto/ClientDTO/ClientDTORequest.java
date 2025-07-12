54784da23102e19a90b76ada6029ce1c734909bc
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-07-12 01:17:16 -0500

    feat (clientDTORequest) : mejorando validaciones.

--- src/main/java/sora/com/saleapi/dto/ClientDTO/ClientDTORequest.java
+++ src/main/java/sora/com/saleapi/dto/ClientDTO/ClientDTORequest.java
@@ -19,23 +19,22 @@ public record ClientDTORequest(
 
         @NotBlank(message = "{field.required}")
         @Size(max = 100, min = 2, message = "{field.size.range}")
-        @Pattern(regexp = "^[\\p{L} ]+$", message = "{field.invalid.format}")
         @Email(message = "{field.email.invalid}")
         String clientEmail,
 
         @NotBlank(message = "{field.required}")
-        @Size(max = 30, min = 2, message = "{field.size.range}")
-        @Pattern(regexp = "^[\\p{L} ]+$", message = "{field.invalid.format}")
+        @Size(max = 30, min = 8, message = "{field.size.range}")
+        @Pattern(regexp = "^\\d{8,30}$", message = "{field.invalid.format}")
         String clientCardId,
 
         @NotBlank(message = "{field.required}")
         @Size(max = 9, min = 9, message = "{field.size.range}")
-        @Pattern(regexp = "^[\\p{L} ]+$", message = "{field.invalid.format}")
+        @Pattern(regexp = "^\\d{9}$", message = "{field.invalid.format}")
         String clientPhone,
 
         @NotBlank(message = "{field.required}")
         @Size(max = 100, min = 2, message = "{field.size.range}")
-        @Pattern(regexp = "^[\\p{L} ]+$", message = "{field.invalid.format}")
+        @Pattern(regexp = "^[\\p{L}\\d ,.#-]+$", message = "{field.invalid.format}")
         String clientAddress
 
 ) {}
