c1a91175c5c5fb4eeb9f7b151ce844ea78cf4774
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-07-15 10:59:23 -0500

    fix (DTO) ProductDTOResponse mejorando format.

--- src/main/java/sora/com/saleapi/dto/ProductDTO/ProductDTOResponse.java
+++ src/main/java/sora/com/saleapi/dto/ProductDTO/ProductDTOResponse.java
@@ -1,5 +1,6 @@
 package sora.com.saleapi.dto.ProductDTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import sora.com.saleapi.dto.CategoryDTO.CategoryDTOResponse;
 
 import java.math.BigDecimal;
@@ -8,6 +9,7 @@ public record ProductDTOResponse(
         Long productId,
         String productName,
         String productDescription,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "0.00")
         BigDecimal productPrice,
         Boolean productEnabled,
         CategoryDTOResponse category
