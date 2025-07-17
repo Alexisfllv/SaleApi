e4eafc3fe2427d1a8010d3577785041ef8137960
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-07-17 11:25:23 -0500

    feat (DTO:sale,saleDetail) : mejorando formato de json BigDecimal.

--- src/main/java/sora/com/saleapi/dto/SaleDetailDTO/SaleDetailDTOResponse.java
+++ src/main/java/sora/com/saleapi/dto/SaleDetailDTO/SaleDetailDTOResponse.java
@@ -1,5 +1,6 @@
 package sora.com.saleapi.dto.SaleDetailDTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import sora.com.saleapi.dto.ProductDTO.ProductDTOResponse;
 import sora.com.saleapi.entity.Product;
 
@@ -7,8 +8,10 @@ import java.math.BigDecimal;
 
 public record SaleDetailDTOResponse(
         Long saleDetailId,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "0.00")
         BigDecimal discount,
         Integer quantity,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "0.00")
         BigDecimal salePrice,
         ProductDTOResponse product,
         Long  saleId
