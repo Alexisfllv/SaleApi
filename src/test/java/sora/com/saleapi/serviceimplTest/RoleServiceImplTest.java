04bf3ab22a44bb43005b61388c78520e8c56b9bd
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-07-21 13:29:09 -0500

    fix (RoleServiceImpl) : DisplayName TestName

--- src/test/java/sora/com/saleapi/serviceimplTest/RoleServiceImplTest.java
+++ src/test/java/sora/com/saleapi/serviceimplTest/RoleServiceImplTest.java
@@ -3,6 +3,8 @@ package sora.com.saleapi.serviceimplTest;
 // static
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -74,227 +76,266 @@ public class RoleServiceImplTest {
 
     }
 
-    // test de listado
-    @Test
-    void givenRoleExist_whenListAll_thenReturnsAllRoles() {
-        // Arrange
-        List<Role> roles = List.of(role1,role2,role3);
-        when(roleRepo.findAll()).thenReturn(roles);
-        when(roleMapper.toRoleDTOResponse(role1)).thenReturn(roleDTOResponse1);
-        when(roleMapper.toRoleDTOResponse(role2)).thenReturn(roleDTOResponse2);
-        when(roleMapper.toRoleDTOResponse(role3)).thenReturn(roleDTOResponse3);
-        // Act
-        List<RoleDTOResponse> result = roleService.findAll();
+    @Nested
+    @DisplayName("findAll()")
+    class FindAll {
+        // test de listado
+        @Test
+        @DisplayName("Should return all roles when findAll is called")
+        void shouldReturnAllRolesWhenFindAll() {
+            // Arrange
+            List<Role> roles = List.of(role1,role2,role3);
+            when(roleRepo.findAll()).thenReturn(roles);
+            when(roleMapper.toRoleDTOResponse(role1)).thenReturn(roleDTOResponse1);
+            when(roleMapper.toRoleDTOResponse(role2)).thenReturn(roleDTOResponse2);
+            when(roleMapper.toRoleDTOResponse(role3)).thenReturn(roleDTOResponse3);
+            // Act
+            List<RoleDTOResponse> result = roleService.findAll();
 
-        // Assert & Verify
-        assertAll(
-                () -> assertNotNull(result),
-                () -> assertEquals(3,result.size()),
-                () -> assertEquals(roleDTOResponse1,result.get(0)),
-                () -> assertEquals(roleDTOResponse2,result.get(1)),
-                () -> assertEquals(roleDTOResponse3,result.get(2))
-        );
+            // Assert & Verify
+            assertAll(
+                    () -> assertNotNull(result),
+                    () -> assertEquals(3,result.size()),
+                    () -> assertEquals(roleDTOResponse1,result.get(0)),
+                    () -> assertEquals(roleDTOResponse2,result.get(1)),
+                    () -> assertEquals(roleDTOResponse3,result.get(2))
+            );
 
-        verify(roleRepo).findAll();
-        verify(roleMapper).toRoleDTOResponse(role1);
-        verify(roleMapper).toRoleDTOResponse(role2);
-        verify(roleMapper).toRoleDTOResponse(role3);
-        verifyNoMoreInteractions(roleRepo,roleMapper);
-    }
+            verify(roleRepo).findAll();
+            verify(roleMapper).toRoleDTOResponse(role1);
+            verify(roleMapper).toRoleDTOResponse(role2);
+            verify(roleMapper).toRoleDTOResponse(role3);
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+        }
 
-    // test listado paginado
-    @Test
-    void givenRolesExist_whenListAllPageable_thenReturnsAllRoles() {
-        // Arrange
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Role> roles = new PageImpl<>(List.of(role1,role2));
-
-        RoleDTOResponse roldto1 = roleDTOResponse1;
-        RoleDTOResponse roldto2 = roleDTOResponse2;
-
-        when(roleRepo.findAll(pageable)).thenReturn(roles);
-        when(roleMapper.toRoleDTOResponse(role1)).thenReturn(roldto1);
-        when(roleMapper.toRoleDTOResponse(role2)).thenReturn(roldto2);
-
-        // Act
-        Page<RoleDTOResponse> result = roleService.findAllPage(pageable);
-
-        // Assert & Verify
-        assertAll(
-                () -> assertEquals(2,result.getContent().size()),
-                () -> assertEquals("ADMIN", result.getContent().get(0).roleName())
-        );
-        verify(roleRepo).findAll(pageable);
-        verify(roleMapper).toRoleDTOResponse(role1);
-        verify(roleMapper).toRoleDTOResponse(role2);
-        verifyNoMoreInteractions(roleRepo,roleMapper);
-    }
-
-    // test de listado retorno vacio
-    @Test
-    void givenListRolesEmpty_whenListAllPageable_thenReturnsEmptyListPage() {
-
-        // Arrange
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Role> roles = new PageImpl<>(List.of());
-        when(roleRepo.findAll(pageable)).thenReturn(roles);
-
-        // Act
-        Page<RoleDTOResponse> result = roleService.findAllPage(pageable);
-        assertAll(
-                () -> assertEquals(0, result.getContent().size())
-        );
-        verify(roleRepo).findAll(pageable);
-        verifyNoMoreInteractions(roleRepo,roleMapper);
 
     }
 
-    // test de busqueda exitoso rol
-    @Test
-    void givenExistRoleId_whenFindById_thenReturnsRoleDTOResponse() {
-        // Arrange
-        Long roleId = 1L;
-        when(roleRepo.findById(roleId)).thenReturn(Optional.of(role1));
-        when(roleMapper.toRoleDTOResponse(role1)).thenReturn(roleDTOResponse1);
+    @Nested
+    @DisplayName("findAllPage(Pageable)")
+    class FindAllPage {
 
-        // Act
-        RoleDTOResponse result = roleService.findById(roleId);
+        // test listado paginado
+        @Test
+        @DisplayName("Should return paged roles when findAllPage is called")
+        void shouldReturnPagedRolesWhenfindAllPage() {
+            // Arrange
+            Pageable pageable = PageRequest.of(0, 10);
+            Page<Role> roles = new PageImpl<>(List.of(role1,role2));
 
-        // Assert & Verify
-        assertAll(
-                () -> assertNotNull(result),
-                () -> assertEquals(roleDTOResponse1,result),
-                () -> assertEquals(roleId,result.roleId()),
-                () -> assertEquals("ADMIN",result.roleName()),
-                () -> assertEquals(true,result.roleEnabled())
-        );
-        verify(roleRepo).findById(roleId);
-        verify(roleMapper).toRoleDTOResponse(role1);
-        verifyNoMoreInteractions(roleRepo,roleMapper);
+            RoleDTOResponse roldto1 = roleDTOResponse1;
+            RoleDTOResponse roldto2 = roleDTOResponse2;
+
+            when(roleRepo.findAll(pageable)).thenReturn(roles);
+            when(roleMapper.toRoleDTOResponse(role1)).thenReturn(roldto1);
+            when(roleMapper.toRoleDTOResponse(role2)).thenReturn(roldto2);
+
+            // Act
+            Page<RoleDTOResponse> result = roleService.findAllPage(pageable);
+
+            // Assert & Verify
+            assertAll(
+                    () -> assertEquals(2,result.getContent().size()),
+                    () -> assertEquals("ADMIN", result.getContent().get(0).roleName())
+            );
+            verify(roleRepo).findAll(pageable);
+            verify(roleMapper).toRoleDTOResponse(role1);
+            verify(roleMapper).toRoleDTOResponse(role2);
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+        }
+
+        // test de listado retorno vacio
+        @Test
+        @DisplayName("Should return empty paged roles when findAllPage is called")
+        void shouldReturnPagedRolesWhenFindAllPage() {
+
+            // Arrange
+            Pageable pageable = PageRequest.of(0, 10);
+            Page<Role> roles = new PageImpl<>(List.of());
+            when(roleRepo.findAll(pageable)).thenReturn(roles);
+
+            // Act
+            Page<RoleDTOResponse> result = roleService.findAllPage(pageable);
+            assertAll(
+                    () -> assertEquals(0, result.getContent().size())
+            );
+            verify(roleRepo).findAll(pageable);
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+
+        }
+
     }
 
-    // test de busqueda rol no existente
-    @Test
-    void giveNonExistRoleId_whenFindById_thenThrowsResourceNotFoundException() {
+    @Nested
+    @DisplayName("findById(Long id)")
+    class FindById {
+        // test de busqueda exitoso rol
+        @Test
+        @DisplayName("Should return role when findById is called")
+        void shouldReturnRoleWhenFindById() {
+            // Arrange
+            Long roleId = 1L;
+            when(roleRepo.findById(roleId)).thenReturn(Optional.of(role1));
+            when(roleMapper.toRoleDTOResponse(role1)).thenReturn(roleDTOResponse1);
 
-        // Arrange
-        Long roleIdNoExist = 99L;
-        when(roleRepo.findById(roleIdNoExist)).thenReturn(Optional.empty());
-        // Act & Assert
-        assertThrows(ResourceNotFoundException.class, () -> roleService.findById(roleIdNoExist));
+            // Act
+            RoleDTOResponse result = roleService.findById(roleId);
 
-        // Verify
-        verify(roleRepo).findById(roleIdNoExist);
-        verifyNoMoreInteractions(roleRepo,roleMapper);
+            // Assert & Verify
+            assertAll(
+                    () -> assertNotNull(result),
+                    () -> assertEquals(roleDTOResponse1,result),
+                    () -> assertEquals(roleId,result.roleId()),
+                    () -> assertEquals("ADMIN",result.roleName()),
+                    () -> assertEquals(true,result.roleEnabled())
+            );
+            verify(roleRepo).findById(roleId);
+            verify(roleMapper).toRoleDTOResponse(role1);
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+        }
+
+        // test de busqueda rol no existente
+        @Test
+        @DisplayName("Should throw ResourceNotFoundException when findById is called with invalid RoleId")
+        void shouldThrowResourceNotFoundWhenFindByIdIsInvalidRoleId() {
+
+            // Arrange
+            Long roleIdNoExist = 99L;
+            when(roleRepo.findById(roleIdNoExist)).thenReturn(Optional.empty());
+            // Act & Assert
+            assertThrows(ResourceNotFoundException.class, () -> roleService.findById(roleIdNoExist));
+
+            // Verify
+            verify(roleRepo).findById(roleIdNoExist);
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+        }
     }
 
+    @Nested
+    @DisplayName("save(dataDTORequest)")
+    class Save {
 
-    // test de registro correcto
-    @Test
-    void givenRoleDTOResponse_whenSave_thenReturnsRoleDTOResponse() {
-        // Arrange
+        // test de registro correcto
+        @Test
+        @DisplayName("Should return created role when save is called")
+        void shouldReturnCreatedRoleWhenSave() {
+            // Arrange
             // valores null
-        RoleDTORequest roleDTOReq =  roleDTORequest1;
-        Role roleReq = roleRequest1;
+            RoleDTORequest roleDTOReq =  roleDTORequest1;
+            Role roleReq = roleRequest1;
 
-        when(roleMapper.toRole(roleDTOReq)).thenReturn(roleReq);
-        when(roleRepo.save(roleReq)).thenReturn(role1);
+            when(roleMapper.toRole(roleDTOReq)).thenReturn(roleReq);
+            when(roleRepo.save(roleReq)).thenReturn(role1);
 
-        when(roleMapper.toRoleDTOResponse(any(Role.class))).thenReturn(roleDTOResponse1);
+            when(roleMapper.toRoleDTOResponse(any(Role.class))).thenReturn(roleDTOResponse1);
 
-        // Act
-        RoleDTOResponse result = roleService.save(roleDTOReq);
+            // Act
+            RoleDTOResponse result = roleService.save(roleDTOReq);
 
-        // Assert & Verify
-        assertAll(
-                () -> assertNotNull(result),
-                () -> assertEquals(1L,result.roleId()),
-                () -> assertEquals("ADMIN",result.roleName()),
-                () -> assertEquals(true,result.roleEnabled())
-        );
-        verify(roleMapper).toRole(roleDTOReq);
-        verify(roleRepo).save(roleReq);
-        verify(roleMapper).toRoleDTOResponse(any(Role.class));
-        verifyNoMoreInteractions(roleRepo,roleMapper);
+            // Assert & Verify
+            assertAll(
+                    () -> assertNotNull(result),
+                    () -> assertEquals(1L,result.roleId()),
+                    () -> assertEquals("ADMIN",result.roleName()),
+                    () -> assertEquals(true,result.roleEnabled())
+            );
+            verify(roleMapper).toRole(roleDTOReq);
+            verify(roleRepo).save(roleReq);
+            verify(roleMapper).toRoleDTOResponse(any(Role.class));
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+        }
+
     }
 
-    // test de modificacion role
-    @Test
-    void givenRoleDTOResponse_whenUpdate_thenReturnsRoleDTOResponse() {
-        // Arrange
-        Long roleId = 1L;
+    @Nested
+    @DisplayName("update(Long id, dataDTORequest)")
+    class Update {
+
+        // test de modificacion role
+        @Test
+        @DisplayName("Should return updated role when update is called")
+        void shouldReturnUpdatedRoleWhenUpdate() {
+            // Arrange
+            Long roleId = 1L;
             // new role
-        RoleDTORequest roleDTOReq =  new RoleDTORequest("SUPER",true);
-        Role roleupdate = new Role(roleId,"SUPER",true);
-        RoleDTOResponse roleDTOResponse = new RoleDTOResponse(1L,"SUPER",true);
+            RoleDTORequest roleDTOReq =  new RoleDTORequest("SUPER",true);
+            Role roleupdate = new Role(roleId,"SUPER",true);
+            RoleDTOResponse roleDTOResponse = new RoleDTOResponse(1L,"SUPER",true);
 
-        when(roleRepo.findById(roleId)).thenReturn(Optional.of(role1));
-        when(roleRepo.save(role1)).thenReturn(roleupdate);
-        when(roleMapper.toRoleDTOResponse(roleupdate)).thenReturn(roleDTOResponse);
+            when(roleRepo.findById(roleId)).thenReturn(Optional.of(role1));
+            when(roleRepo.save(role1)).thenReturn(roleupdate);
+            when(roleMapper.toRoleDTOResponse(roleupdate)).thenReturn(roleDTOResponse);
 
-        // Act
-        RoleDTOResponse resul = roleService.update(roleId,roleDTOReq);
+            // Act
+            RoleDTOResponse resul = roleService.update(roleId,roleDTOReq);
 
-        // Assert & Verify
-        assertAll(
-                () -> assertNotNull(resul),
-                () -> assertEquals(roleId,resul.roleId()),
-                () -> assertEquals("SUPER",resul.roleName()),
-                () -> assertEquals(true,resul.roleEnabled())
-        );
-        verify(roleRepo).findById(roleId);
-        verify(roleRepo).save(role1);
-        verify(roleMapper).toRoleDTOResponse(roleupdate);
+            // Assert & Verify
+            assertAll(
+                    () -> assertNotNull(resul),
+                    () -> assertEquals(roleId,resul.roleId()),
+                    () -> assertEquals("SUPER",resul.roleName()),
+                    () -> assertEquals(true,resul.roleEnabled())
+            );
+            verify(roleRepo).findById(roleId);
+            verify(roleRepo).save(role1);
+            verify(roleMapper).toRoleDTOResponse(roleupdate);
+        }
+
+        // test modificar con id inexistente
+        @Test
+        @DisplayName("Should throw ResourceNotFoundException when update is called with invalid RoleId")
+        void shouldThrowNotFoundWhenUpdateIsCalledWithInvalidRoleId() {
+            // Arrange
+            Long roleIdNoExist = 99L;
+            RoleDTORequest roleDTOReq =  new RoleDTORequest("SUPER",true);
+
+            when(roleRepo.findById(roleIdNoExist)).thenReturn(Optional.empty());
+
+            // Act & Assert
+            assertThrows(ResourceNotFoundException.class, () -> roleService.update(roleIdNoExist,roleDTOReq));
+
+            // Verify
+            verify(roleRepo).findById(roleIdNoExist);
+            verifyNoMoreInteractions(roleRepo,roleMapper);
+        }
+
     }
 
-    // test modificar con id inexistente
-    @Test
-    void givenNonExistingRoleId_whenUpdate_thenThrowsResourceNotFoundException() {
-        // Arrange
-        Long roleIdNoExist = 99L;
-        RoleDTORequest roleDTOReq =  new RoleDTORequest("SUPER",true);
+    @Nested
+    @DisplayName("deleteById(Long id)")
+    class DeleteById {
 
-        when(roleRepo.findById(roleIdNoExist)).thenReturn(Optional.empty());
+        // test de eliminar un rol
+        @Test
+        @DisplayName("Should delete role when deleteById is called")
+        void shouldReturnDeletedRoleWhenDeleteById() {
+            // Arrange
+            Long roleId = 1L;
+            Role rolexist = role1;
 
-        // Act & Assert
-        assertThrows(ResourceNotFoundException.class, () -> roleService.update(roleIdNoExist,roleDTOReq));
+            when(roleRepo.findById(roleId)).thenReturn(Optional.of(rolexist));
+            // Act
+            roleService.deleteById(roleId);
 
-        // Verify
-        verify(roleRepo).findById(roleIdNoExist);
-        verifyNoMoreInteractions(roleRepo,roleMapper);
+            // Assert & Verify
+            verify(roleRepo).findById(roleId);
+            verify(roleRepo).delete(rolexist);
+            verifyNoMoreInteractions(roleRepo);
+        }
+
+        // test de eliminar un rol no existente
+        @Test
+        @DisplayName("Should throw ResourceNotFoundException when deleteById is called with invalid RoleId")
+        void shouldThrowNotFoundWhenDeleteByIdIsCalledWithInvalidRoleId() {
+            // Arrange
+            Long roleIdNoExist = 99L;
+            when(roleRepo.findById(roleIdNoExist)).thenReturn(Optional.empty());
+            // Act & Assert
+            assertThrows(ResourceNotFoundException.class, () -> roleService.deleteById(roleIdNoExist));
+
+            // Verify
+            verify(roleRepo).findById(roleIdNoExist);
+            verifyNoMoreInteractions(roleRepo);
+        }
     }
-
-    // test de eliminar un rol
-    @Test
-    void givenExistRoleId_whenDelete_thenReturnsIsDeleted() {
-        // Arrange
-        Long roleId = 1L;
-        Role rolexist = role1;
-
-        when(roleRepo.findById(roleId)).thenReturn(Optional.of(rolexist));
-        // Act
-        roleService.deleteById(roleId);
-
-        // Assert & Verify
-        verify(roleRepo).findById(roleId);
-        verify(roleRepo).delete(rolexist);
-        verifyNoMoreInteractions(roleRepo);
-    }
-
-    // test de eliminar un rol no existente
-    @Test
-    void givenNonExistingRoleId_whenDelete_thenThrowsResourceNotFoundException() {
-        // Arrange
-        Long roleIdNoExist = 99L;
-        when(roleRepo.findById(roleIdNoExist)).thenReturn(Optional.empty());
-        // Act & Assert
-        assertThrows(ResourceNotFoundException.class, () -> roleService.deleteById(roleIdNoExist));
-
-        // Verify
-        verify(roleRepo).findById(roleIdNoExist);
-        verifyNoMoreInteractions(roleRepo);
-    }
-
-
-
 }
